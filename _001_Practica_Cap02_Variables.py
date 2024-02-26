1f014bf6551bc0562438b98ef937f0daf46f10fc
Robert-MTZ <160370342+Robert-MTZ@users.noreply.github.com>
2024-02-25 19:47:33 -0600

    Create _001_Practica_Cap02_Variables.py
    Practica para aprender a como utilizar las variables en Python

---
+++ _001_Practica_Cap02_Variables.py
@@ -0,0 +1,10 @@
+# Roberto Martínez Bailón  21310216 Inteligencia Artificial 
+# Practica_Cap02_Variables
+
+
+num1 = 40 # Se le da un valor a la primera variable
+num2 = 60 # Se le da un valor a la segunda variable 
+
+res = num1 + num2 # Se suman los valores de las variables anteriores
+
+print(res) # Se imprime el valor de la variable 
