a45cee8c0c48ac6106e7c1db666a41462e817e4e
Robert-MTZ <160370342+Robert-MTZ@users.noreply.github.com>
2024-02-25 20:02:11 -0600

    mas programas

---
+++ RobertoMartinezBailon/_002_Practica_Cap03_strings.py
@@ -0,0 +1,6 @@
+# Roberto Martínez Bailón  21310216 Inteligencia Artificial 
+# Practica_Cap03_Strings
+
+mensaje = '"print()" mensaje en la consola' # Se utiliza para imprimir la cadena de caracteres
+
+print(mensaje) # Se imprime el mensaje almacenado en la variable 
---
+++ RobertoMartinezBailon/_003_Practica_Cap04_Concatenar.py
@@ -0,0 +1,40 @@
+# Roberto Martínez Bailón  21310216 Inteligencia Artificial 
+# Practica_Cap04_Concatenar 
+
+# Ejercicio 7
+concatena = 'Roberto ' + 'Martinez Bailon.' # Unimos dos strings por medio del operador de suma
+
+print(concatena) # imprimimos el valor de la variable ya concatenado
+
+# Ejercicio 8 
+palabra01 = 'Roberto ' # declaramos en una variable mi nombre
+palabra02 = 'Martinez' # declaramos en una segunda variable mi primer apellido
+
+name = palabra01 + palabra02 # creamos una variable en donde se uniran las dos variables anteriores
+
+print(name) # imprimimos la variable que almacena el resultado 
+
+#Ejercicio 9 
+palabra1 = "Roberto " # le damos un valor a la variable
+
+print(palabra1 + "Martinez") # la imprimimos junto con un mensaje adicional 
+
+#Ejercicio 10
+nombre = "Roberto " # se crea una variable que almacene el nombre 
+apellido01 = "Martinez " # se crea una variable que almacene el primer apellido
+apellido02 = "Bailon " # se crea una variable que almacene el segundo apellido
+
+nombreC = nombre + apellido01 + apellido02 # se crea una variable que almacene el resultado de las variables anteriores
+
+print(nombreC) # se imprime la variable que almacena el resultado 
+
+#Ejercicio 11
+numero01 = "10 " # se le asigna un valor numerico a la primera variable
+numero02 = "11 " # se le asigna un valor numerico a la segunda variable
+
+numero03 = numero01 + numero02 # se suman las dos variables anteriores y se almacenan en una sola variable
+
+print(numero03) # se imprime el resultado de la variable que almacena el resultado 
+
+
+
