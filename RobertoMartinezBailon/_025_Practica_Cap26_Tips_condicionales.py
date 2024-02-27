c5add4e4a81d414db0de5b45be08490b74d2612d
Robert-MTZ <160370342+Robert-MTZ@users.noreply.github.com>
2024-02-27 00:03:28 -0600

    programa nuevo

---
+++ RobertoMartinezBailon/_025_Practica_Cap26_Tips_condicionales.py
@@ -0,0 +1,12 @@
+# Roberto Martínez Bailón  21310216 Inteligencia Artificial 
+# Practica_Cap26_Tips_condicionales
+
+# Se crean variables y se les asignan valores 
+a = 100
+b = 600
+c = 900
+
+if a < b and c > b: # Con este if se comprueba si se cumplen los parametros deseados
+    print("Se cumplen tus deseos") # Se imprime si se cumplen los parametros
+else:
+    print("No se cumplen tus sueños") # Se imprime si no se cumplen
---
+++ RobertoMartinezBailon/_026_Practica_Cap27_Bucle_while.py
@@ -0,0 +1,29 @@
+# Roberto Martínez Bailón  21310216 Inteligencia Artificial 
+# Practica_Cap27_Bucle_while
+
+# Ejercicio 01
+print ("Ejercicio 01\n")
+
+a = 0 # creamos una variable que inicie en cero
+
+while a <= 80: # ponemos un while, el cual nos indicara hasta donde llegara nuestro numero
+   print(a) # este print nos permitira ver todos los numeros por los que se ira incrementando nuestra variable
+   a += 4 # establecemos como ira incrementando en este caso sera de 4 en 4
+
+# Ejercicio 02
+print ("Ejercicio 02\n")
+
+b = 0 # creamos una variable que inicie en cero
+
+while b >= -200: # ponemos un while, el cual nos indicara hasta donde llegara nuestro numero
+   print(b) # este print nos permitira ver todos los numeros por los que se ira decrementando nuestra variable
+   b -= 25 # establecemos como ira decrementando en este caso sera de 25 en 25
+
+# Ejercicio 03
+print("Ejercicio 03\n")
+
+c = 200 # creamos una variable que inicie en cero
+
+while c >= 0: # ponemos un while, el cual nos indicara hasta donde llegara nuestro numero
+    print("El monto de c es: ", c) # este print nos permitira ver todos los numeros por los que se ira decrementando nuestra variable
+    c -= 10 # establecemos como ira decrementando en este caso sera de 10 en 10
